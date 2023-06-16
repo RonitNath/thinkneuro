2e9561a77ac817a4197a8b23618d81bc07cfa5d9
Ronit Nath <ronitnath@berkeley.edu>
2023-06-16 13:40:35 -0700

    added gitignore and changed nav to navlink component

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -5,20 +5,28 @@
     <p>ThinkNeuro is a wonderful organization</p>
     <nav>
 
-        <Navlink to />
+        <Navlink to="/" name="Home" />
+        <Navlink to="/about" name="About" />
+        <Navlink to="/team" name="Team" />
+        <Navlink to="/outreach" name="Community Engagement" />
+        <Navlink to="/contact" name="Contact" />
+        <Navlink to="/resources" name="Resources" />
+        <Navlink to="/faq" name="FAQ" />
+        <Navlink to="/curriculum" name="Curriculum" />
 
-        <a href="/" use:link >Home</a>
+        <!-- <a href="/" use:link >Home</a>
         <a href="/about" use:link >About</a>
         <a href="/team" use:link>Team</a>
         <a href="/outreach" use:link>Community Engagement</a>
         <a use:link href="/contact">Contact</a>
         <a use:link href="/resources">Resources</a>
         <a use:link href="/faq">FAQ</a>
-        <a use:link href="/curriculum">Curriculum</a>
+        <a use:link href="/curriculum">Curriculum</a> -->
 
     </nav>
 </header>
 
 <script>
     import Navlink from "./navlink.svelte";
+    import { link } from "svelte-spa-router";
 </script>
